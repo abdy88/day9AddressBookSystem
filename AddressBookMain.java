5cad84407e8843cdacf02b9c7298252bd69ba285
aytech11 <aytech0110@gmail.com>
2022-09-14 18:03:37 +0530

    Welcome to address Book program

---
+++ AddressBookMain.java
@@ -0,0 +1,14 @@
+package bl.com.day9addressbooksystem;
+
+public class AddressBookMain {
+	
+	
+	public static void main(String[] args) {
+	
+	
+		System.out.println("Welcome to Address Book Program");
+	
+	
+	}
+
+}
